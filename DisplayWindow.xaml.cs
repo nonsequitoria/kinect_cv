28a45f1c6443556b3d1bc7d39af5d5e5f33fb5d8
nonsequitoria <dan@nonsequitoria.com>
2014-04-23 22:07:06 -0400

    initial commit

---
+++ DisplayWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+using Microsoft.Kinect;
+
+namespace kinect_cv
+{
+    /// <summary>
+    /// Interaction logic for DisplayWindow.xaml
+    /// </summary>
+    public partial class DisplayWindow : Window
+    {
+
+
+        private byte[] colorPixels;
+        private DepthImagePixel[] depthPixels;
+
+        Emgu.CV.Image<Bgr, byte> emguColour;
+
+        public DisplayWindow(KinectSensor sensor)
+        {
+            InitializeComponent();
+
+            // Allocate space to put the depth pixels we'll receive
+            depthPixels = new DepthImagePixel[sensor.DepthStream.FramePixelDataLength];
+
+            // Allocate space to put the color pixels we'll create
+            colorPixels = new byte[sensor.ColorStream.FramePixelDataLength];
+
+
+
+        }
+
+        public void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            bool depthReceived = false;
+            bool colorReceived = false;
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (null != depthFrame)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
+
+                    depthReceived = true;
+                }
+            }
+
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (null != colorFrame)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    colorFrame.CopyPixelDataTo(this.colorPixels);
+                    
+                    colorReceived = true;
+                }
+            }
+
+
+            if (colorReceived != null)
+            {
+                //emguColour = new Image<Bgr, byte>(ToBitmap(colorPixels, 640, 480));
+
+            }
+
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using System.IO;
+using System.Diagnostics;
+
+using System.Runtime.InteropServices;
+
+
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace kinect_cv
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// stream formats
+        private const DepthImageFormat DepthFormat = DepthImageFormat.Resolution320x240Fps30;
+        private const ColorImageFormat ColorFormat = ColorImageFormat.RgbResolution640x480Fps30;
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor sensor;
+
+        /// <summary>
+        /// Bitmap that will hold color information
+        /// </summary>
+        private WriteableBitmap colorBitmap;
+
+        /// <summary>
+        /// Bitmap that will hold opacity mask information
+        /// </summary>
+        private WriteableBitmap playerOpacityMaskImage = null;
+
+        /// <summary>
+        /// Intermediate storage for the depth data received from the sensor
+        /// </summary>
+        private DepthImagePixel[] depthPixels;
+
+        /// <summary>
+        /// Intermediate storage for the color data received from the camera
+        /// </summary>
+        private byte[] colorPixels;
+
+        /// <summary>
+        /// Intermediate storage for the player opacity mask
+        /// </summary>
+        private int[] playerPixelData;
+
+        /// <summary>
+        /// Intermediate storage for the depth to color mapping
+        /// </summary>
+        private ColorImagePoint[] colorCoordinates;
+
+        /// <summary>
+        /// Inverse scaling factor between color and depth
+        /// </summary>
+        private int colorToDepthDivisor;
+
+        /// depth image size
+        private int depthWidth;
+        private int depthHeight;
+
+        /// Indicates opaque in an opacity mask
+        private int opaquePixelValue = -1;
+
+
+        private InteropBitmapHelper mColorImageHelper = null;
+
+        MCvFont debugFont = new MCvFont(FONT.CV_FONT_HERSHEY_PLAIN, 1.0, 1.0); //Create the font
+
+        Image<Bgr, byte> colourImage;
+
+        //DisplayWindow displayWindow;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Debug.WriteLine("Window_Loaded");
+
+
+           
+
+
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the depth stream to receive depth frames
+                this.sensor.DepthStream.Enable(DepthFormat);
+
+                this.depthWidth = this.sensor.DepthStream.FrameWidth;
+
+                this.depthHeight = this.sensor.DepthStream.FrameHeight;
+
+                this.sensor.ColorStream.Enable(ColorFormat);
+
+                int colorWidth = this.sensor.ColorStream.FrameWidth;
+                int colorHeight = this.sensor.ColorStream.FrameHeight;
+
+                this.colorToDepthDivisor = colorWidth / this.depthWidth;
+
+                // Turn on to get player masks
+                this.sensor.SkeletonStream.Enable();
+
+                // Allocate space to put the depth pixels we'll receive
+                this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+                // Allocate space to put the color pixels we'll create
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+
+                this.playerPixelData = new int[this.sensor.DepthStream.FramePixelDataLength];
+
+                this.colorCoordinates = new ColorImagePoint[this.sensor.DepthStream.FramePixelDataLength];
+
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap = new WriteableBitmap(colorWidth, colorHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                this.MaskedColor.Source = this.colorBitmap;
+
+                // event handler when frames are ready
+                this.sensor.AllFramesReady += this.SensorAllFramesReady;
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+            if (null == this.sensor)
+            {
+                this.statusBarText.Text = "Kinect Not Ready";
+            }
+            else
+            {
+                statusBarText.Text = String.Format("{0}", sensor.UniqueKinectId);
+
+                // create the display window
+                //displayWindow = new DisplayWindow(sensor);
+                //displayWindow.Show();
+
+                // Add an event handler to be called whenever there is new depth frame data
+                //sensor.AllFramesReady += displayWindow.SensorAllFramesReady;
+
+            }
+
+            // Emgu window setup
+            CvInvoke.cvNamedWindow(win1); //Create the window using the specific name
+        }
+
+
+        String win1 = "Emgu Window"; //The name of the window
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Debug.WriteLine("Window_Closing");
+
+            //sensor.AllFramesReady -= displayWindow.SensorAllFramesReady;
+           // displayWindow.Close();
+            //displayWindow = null;
+
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+                this.sensor = null;
+            }
+
+            // Emgu window shutdown
+            //CvInvoke.cvWaitKey(0);  //Wait for the key pressing event
+            CvInvoke.cvDestroyWindow(win1); //Destory the window
+        }
+
+
+        private void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            // if in the middle of shutting down, so nothing to do
+            if (null == this.sensor) return;
+
+            bool depthReceived = false;
+            bool colorReceived = false;
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (null != depthFrame)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
+
+                    depthReceived = true;
+                }
+            }
+
+            int colorFrameNum = -1;
+
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (null != colorFrame)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    colorFrame.CopyPixelDataTo(this.colorPixels);
+                    colorFrameNum = colorFrame.FrameNumber;
+                    colorReceived = true;
+                }
+            }
+
+
+            if (true == colorReceived)
+            {
+
+
+                colourImage = new Image<Bgr, byte>(ToBitmap(colorPixels,
+                    sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight));
+
+               
+                String debugMsg = String.Format("{0}", colorFrameNum);
+                colourImage.Draw(debugMsg, ref debugFont, new System.Drawing.Point(10, 80), new Bgr(255, 255, 255));
+
+
+                CvInvoke.cvShowImage(win1, colourImage);
+
+                // Write the pixel data into our bitmap
+                //this.colorBitmap.WritePixels(
+                //    new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                //    this.colourImage.Bytes,
+                //    this.colorBitmap.PixelWidth * sizeof(int),
+                //    0);
+
+                //An interopBitmap is a WPF construct that enables resetting the Bits of the image.
+                //This is more efficient than doing a BitmapSource.Create call every frame.
+                if (mColorImageHelper == null)
+                {
+                    mColorImageHelper = new InteropBitmapHelper(colourImage.Width, colourImage.Height, colourImage.Bytes, PixelFormats.Bgr24);
+                    MaskedColor.Source = mColorImageHelper.InteropBitmap;
+                }
+
+                mColorImageHelper.UpdateBits(colourImage.Bytes);
+
+            }
+
+        }
+
+
+        public static System.Drawing.Bitmap ToBitmap(byte[] pixels, int width, int height)
+        {
+            return ToBitmap(pixels, width, height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+        }
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        public static System.Drawing.Bitmap ToBitmap(byte[] pixels, int width, int height, System.Drawing.Imaging.PixelFormat format)
+        {
+            if (pixels == null)
+                return null;
+
+            var bitmap = new System.Drawing.Bitmap(width, height, format);
+
+            var data = bitmap.LockBits(
+                new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bitmap.PixelFormat);
+
+            Marshal.Copy(pixels, 0, data.Scan0, pixels.Length);
+
+            bitmap.UnlockBits(data);
+
+            return bitmap;
+        }
+
+    }
+}
